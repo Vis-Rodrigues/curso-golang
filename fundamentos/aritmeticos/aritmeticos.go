36a3d6ed6122df737b62d93629cf1d21e18636c9
Vis-Rodrigues <vi_ale10@hotmail.com>
2025-03-04 20:21:40 -0300

    aulas de operadores relacionais e atribuicao

--- fundamentos/aritmeticos/aritmeticos.go
+++ fundamentos/aritmeticos/aritmeticos.go
@@ -18,7 +18,12 @@ func main() {
 	// bitwise
 	fmt.Println("E =>", a&b)   // 11 & 10 = 10
 	fmt.Println("OU =>", a|b)  // 11 | 10 = 11
-	fmt.Println("Xor =>", a^b) // 11 ^ 10 = 01
+	fmt.Println("Xor =>", a^b) // 11 ^ 10 = 01 // operação de XOR bit a bit (ou "exclusive or")
+
+	x := 5          // 0101 em binário
+	z := 3          // 0011 em binário
+	result := x ^ z // Resultado: 6 (0110 em binário
+	fmt.Println(result)
 
 	c := 3.0
 	d := 2.0
